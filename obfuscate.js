82e4a0c59eacc7af3f873d84168b99f4964c463e
Menajul Hoque <menajulhoque99@gmail.com>
2024-04-04 00:52:26 +0530

    Update obfuscate.js

--- obfuscate.js
+++ obfuscate.js
@@ -1,11 +1,1 @@
-const fs = require('fs');
-const crypto = require('crypto');
-
 const API_KEY = 'AIzaSyBq6CK24S5pm6mPdc_dfdbbPBpiSb_kvYo'; // Replace with your actual API key
-
-const obfuscatedKey = crypto.createHash('sha256').update(API_KEY).digest('hex');
-
-const indexHtml = fs.readFileSync('dist/index.html', 'utf8');
-const obfuscatedHtml = indexHtml.replace('const API_KEY = undefined;', `const API_KEY = '${obfuscatedKey}';`);
-
-fs.writeFileSync('dist/index.html', obfuscatedHtml);
